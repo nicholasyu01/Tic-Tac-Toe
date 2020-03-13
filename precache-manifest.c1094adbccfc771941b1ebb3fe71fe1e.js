47a64a854314b97de9bbbaf66136c4434e52a907
Nicholas Yu <nyu@shaw.ca>
2020-03-13 00:42:04 -0700

    Updates

---
+++ precache-manifest.c1094adbccfc771941b1ebb3fe71fe1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bcfa3f57bc6486b329e78c79188b93",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "cd3c6fa2638e5e50c389",
+    "url": "/Tic-Tac-Toe/static/css/main.793b2eaa.chunk.css"
+  },
+  {
+    "revision": "badb2636bd56744507fa",
+    "url": "/Tic-Tac-Toe/static/js/2.417f3472.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Tic-Tac-Toe/static/js/2.417f3472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3c6fa2638e5e50c389",
+    "url": "/Tic-Tac-Toe/static/js/main.de8431b7.chunk.js"
+  },
+  {
+    "revision": "b517681dcc694863457a",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.005d7142.js"
+  }
+]);
